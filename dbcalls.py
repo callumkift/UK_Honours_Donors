5ec47fba912613cf6ca89f67847052656c4598ae
Callum Kift <callumkift@gmail.com>
2015-08-25 18:38:24 +0200

    Inserts honour type into table.

--- dbcalls.py
+++ dbcalls.py
@@ -24,9 +24,11 @@ def createdb():
         c = conn.cursor()
 
         # Honour List Tables
-        c.execute("CREATE TABLE HonourType(id INTEGER PRIMARY KEY, h_order TEXT, award TEXT, level TEXT)")
+        c.execute('''CREATE TABLE HonourType(id INTEGER PRIMARY KEY, h_order TEXT, award TEXT, level TEXT,
+                        UNIQUE(h_order, award, level))''')
 
-        c.execute('''CREATE TABLE HonourList(id INTEGER PRIMARY KEY, list TEXT, year TEXT)''')
+        c.execute('''CREATE TABLE HonourList(id INTEGER PRIMARY KEY, list TEXT, year TEXT,
+                        UNIQUE(list, year))''')
 
         c.execute('''CREATE TABLE HonourPerson(id INTEGER PRIMARY KEY, name TEXT, citation TEXT, county TEXT,
                         hl_id INTEGER, ht_id INTEGER, FOREIGN KEY(hl_id) REFERENCES HonourList(id),
@@ -66,8 +68,25 @@ def addhon(hondict):
     :param hondict: Dictionary of honours information
     :return:
     """
+
+    name = hondict.values()[0]
+    level = hondict.values()[1]
+    citation = hondict.values()[2]
+    list = hondict.values()[3]
+    award = hondict.values()[4]
+    county = hondict.values()[5]
+    year = hondict.values()[6]
+    order = hondict.values()[7]
+
     conn = connect()
     c = conn.cursor()
 
+    for i in range(len(name)):
+        try:
+            c.execute(''' INSERT INTO HonourType(h_order, award, level) VALUES(?,?,?)
+                    ''', (order[i], award[i], level[i], ))
+        except sqlite3.IntegrityError as e:
+            pass
+    conn.commit()
     conn.close()
     return
